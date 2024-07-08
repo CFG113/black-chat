0ef7fe6056e1ba43f49aa9e4963b4752fa59bf6d
Chr1stian234 <s3892544@student.rmit.edu.au>
2024-07-08 14:20:11 +1000

    socket io for communication

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+# app.py
 from flask import Flask
 from flask_cors import CORS
+from flask_socketio import SocketIO, send
 
 from controllers.link_controller import link_bp
 from controllers.session_controller import session_bp
@@ -9,5 +11,14 @@ CORS(app)
 app.register_blueprint(session_bp)
 app.register_blueprint(link_bp)
 
+# Initialize SocketIO
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+# Define WebSocket event for handling messages
+@socketio.on('message')
+def handle_message(msg):
+    print(f'Message: {msg}')
+    send(msg, broadcast=True)
+
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    socketio.run(app, port=5000, debug=True)
